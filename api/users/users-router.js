26e2a745b2a5fdd5f17ebadc7c0e84bf755c3a2c
Pauline122112 <pauline.design007@gmail.com>
2021-09-29 18:17:29 -0500

    Written the middleware

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,22 +1,51 @@
 const express = require('express');
+const {
+	validateUserId,
+	// validateUser,
+	validatePost,
+} = require("../middleware/middleware.js");
+const Users = require('./users-model')
+const Posts = require("../posts/posts-model");
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // RETURN AN ARRAY WITH ALL THE USERS
+router.get("/", (req, res) => {
+	Users.get(req.query)
+		.then((users) => {
+			res.status(200).json(users);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
 });
 
-router.get('/:id', (req, res) => {
-  // RETURN THE USER OBJECT
-  // this needs a middleware to verify user id
+router.post("/:id/posts", validateUserId,(req, res) => {
+	// RETURN THE NEWLY CREATED USER POST
+	// this needs a middleware to verify user id
+	// and another middleware to check that the request body is valid
+	const postInfo = { ...req.body, user_id: req.params.id };
+	Posts.insert(postInfo)
+		.then((post) => {
+			res.status(200).json(post);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post("/", validatePost, (req, res) => {
+	// RETURN THE NEWLY CREATED USER OBJECT
+	// this needs a middleware to check that the request body is valid
+	Posts.insert(req.body)
+		.then((post) => {
+			res.status(201).json(post);
+		})
+		.catch((error) => {
+			res.status(500).json({ message: error.message });
+		});
 });
 
 router.put('/:id', (req, res) => {
@@ -42,3 +71,4 @@ router.post('/:id/posts', (req, res) => {
 });
 
 // do not forget to export the router
+module.exports = router
