db01538b1d93bc6ec7bbfb500cc81a2e76d326da
Pauline122112 <pauline.design007@gmail.com>
2021-09-29 14:12:24 -0500

    Creating the index and server js files to get the port running

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-
+const express = require("express")
 const server = express();
+// const usersRouter = require("./users/users-router")
 
-// remember express by default cannot parse JSON in request bodies
 
-// global middlewares and the user's router need to be connected here
+server.use(express.json())
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
 
-module.exports = server;
+
+// server.use("./api/users", usersRouter)
+
+
+
+server.get("/", (req, res) => {
+	res.send(`<h2>Let's write some middleware!</h2>`)
+})
+
+module.exports = server
